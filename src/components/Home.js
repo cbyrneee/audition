028d7c90fa6df099f0484c02eb443878f855c69d
Conor Byrne <71222289+dreamhopping@users.noreply.github.com>
2021-05-02 12:45:24 +0100

    [Components: Home + Artist] Clean up API code
    This commit cleans up the useEffect in HomeComponent by using the
    async/await syntax. This makes the code a lot easier to read!
    
    The Artist component has also been updated to support these changes.

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,63 +16,32 @@ export default function HomeComponent({ code }) {
     const accessToken = useSpotifyAuth(code);
 
     useEffect(() => {
-        if (!accessToken) return;
+        async function getData() {
+            if (!accessToken) return;
+            spotifyAPI.setAccessToken(accessToken);
 
-        spotifyAPI.setAccessToken(accessToken);
-        spotifyAPI.getMe().then((data) => {
-            setUsername(data.body.display_name || "you");
-        });
+            const topArtists = (
+                await spotifyAPI.getMyTopArtists()
+            ).body.items.map((item) => item.id);
 
-        spotifyAPI.getMyTopArtists().then((data) => {
-            const topArtists = data.body.items
-                .map((artist) => {
-                    return artist.id;
-                })
-                .slice(0, 5);
+            const recommendations = await spotifyAPI.getRecommendations({
+                min_energy: 0.0,
+                seed_artists: topArtists.slice(0, 5),
+                min_popularity: 0.0,
+            });
 
-            spotifyAPI
-                .getRecommendations({
-                    min_energy: 0.0,
-                    seed_artists: topArtists.slice(0, 5),
-                    min_popularity: 0.0,
-                })
-                .then((data) => {
-                    const existingArtists = {};
-                    const artistIDs = data.body.tracks
-                        .filter((item) => {
-                            // Remove duplicates
-                            const exists = existingArtists[item.artists[0].id];
-                            if (!exists) {
-                                existingArtists[item.artists[0].id] = true;
-                            }
+            const recommendedArtists = await spotifyAPI.getArtists(
+                recommendations.body.tracks.map((track) => track.artists[0].id)
+            );
 
-                            return !exists;
-                        })
-                        .map((track) => {
-                            return track.artists[0].id;
-                        });
+            const me = (await spotifyAPI.getMe()).body;
+            setUsername(me.display_name || "you");
 
-                    spotifyAPI.getArtists(artistIDs).then((data) => {
-                        setRecommendedArtists(
-                            data.body.artists.map((artist) => {
-                                return {
-                                    id: artist.id,
-                                    name: artist.name,
-                                    url: artist.external_urls.spotify,
-                                    image:
-                                        artist.images.length >= 0
-                                            ? artist.images[0].url
-                                            : "",
-                                };
-                            })
-                        );
+            setRecommendedArtists(recommendedArtists.body.artists);
+            setLoaded(true);
+        }
 
-                        setLoaded(true);
-                    });
-                });
-        });
-
-        return () => {};
+        getData();
     }, [accessToken]);
 
     return (
