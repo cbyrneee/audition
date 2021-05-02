b86bbc6258f1a88901ac2e66c3e90987e667b3f8
Conor Byrne <71222289+dreamhopping@users.noreply.github.com>
2021-05-02 11:56:00 +0100

    [UI] Add GitHub button & add icons to buttons

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { SPOTIFY_CLIENT_ID, SPOTIFY_REDIRECT_URI } from "../constants";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faSpotify } from "@fortawesome/free-brands-svg-icons";
 
 export default function LoginComponent() {
     return (
@@ -11,12 +13,26 @@ export default function LoginComponent() {
                 artists
             </p>
 
-            <a
-                className="btn btn-primary"
-                href={`https://accounts.spotify.com/authorize?client_id=${SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${SPOTIFY_REDIRECT_URI}&scope=user-read-email%20user-read-private%20user-top-read&show_dialog=true`}
-            >
-                Login with Spotify
-            </a>
+            <div>
+                <a
+                    className="btn btn-primary"
+                    href={`https://accounts.spotify.com/authorize?client_id=${SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${SPOTIFY_REDIRECT_URI}&scope=user-read-email%20user-read-private%20user-top-read&show_dialog=true`}
+                >
+                    <FontAwesomeIcon icon={faSpotify} />
+                    <span style={{ marginLeft: "8px" }}>
+                        Login with Spotify
+                    </span>
+                </a>
+                <a
+                    className="btn btn-github"
+                    href={"https://github.com/dreamhopping/audition"}
+                >
+                    <FontAwesomeIcon icon={faGithub} />
+                    <span style={{ marginLeft: "8px" }}>
+                        View source on GitHub
+                    </span>
+                </a>
+            </div>
         </div>
     );
 }
