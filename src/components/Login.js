3b7e77697672aabb4deec16a0532d87655cd970e
Conor Byrne <71222289+dreamhopping@users.noreply.github.com>
2021-05-02 12:05:00 +0100

    [UI] Improve responsiveness

--- src/components/Login.js
+++ src/components/Login.js
@@ -18,19 +18,15 @@ export default function LoginComponent() {
                     className="btn btn-primary"
                     href={`https://accounts.spotify.com/authorize?client_id=${SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${SPOTIFY_REDIRECT_URI}&scope=user-read-email%20user-read-private%20user-top-read&show_dialog=true`}
                 >
-                    <FontAwesomeIcon icon={faSpotify} />
-                    <span style={{ marginLeft: "8px" }}>
-                        Login with Spotify
-                    </span>
+                    <FontAwesomeIcon className="fa-icon" icon={faSpotify} />
+                    <span>Login with Spotify</span>
                 </a>
                 <a
                     className="btn btn-github"
                     href={"https://github.com/dreamhopping/audition"}
                 >
-                    <FontAwesomeIcon icon={faGithub} />
-                    <span style={{ marginLeft: "8px" }}>
-                        View source on GitHub
-                    </span>
+                    <FontAwesomeIcon className="fa-icon" icon={faGithub} />
+                    <span>View source on GitHub</span>
                 </a>
             </div>
         </div>
