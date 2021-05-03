82b701285142188ce476d5ac6624c85e0adc03ec
Conor Byrne <71222289+dreamhopping@users.noreply.github.com>
2021-05-03 10:35:41 +0100

    [Misc] Change URL consts depending on environment

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,10 @@
+const IS_DEVELOPMENT_ENV =
+    !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+
 export const SPOTIFY_CLIENT_ID = "0484168bada84e948070230150fddf2d";
-export const SPOTIFY_REDIRECT_URI = "https://audition.vercel.app";
-export const API_URL = "https://audition-serverless.vercel.app";
+export const SPOTIFY_REDIRECT_URI = IS_DEVELOPMENT_ENV
+    ? "http://localhost:3000"
+    : "https://audition.vercel.app";
+export const API_URL = IS_DEVELOPMENT_ENV
+    ? "http://localhost:3001"
+    : "https://audition-serverless.vercel.app";
