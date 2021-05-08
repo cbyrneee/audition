0de76cd49a6730b50bd78ae4a59301fb9949941a
Conor Byrne <71222289+dreamhopping@users.noreply.github.com>
2021-05-08 11:49:41 +0100

    [Meta] Update API_URL and REDIRECT_URI

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@ const IS_DEVELOPMENT_ENV =
 export const SPOTIFY_CLIENT_ID = "0484168bada84e948070230150fddf2d";
 export const SPOTIFY_REDIRECT_URI = IS_DEVELOPMENT_ENV
     ? "http://localhost:3000"
-    : "https://audition.vercel.app";
+    : "https://audition.cbyrne.dev";
 export const API_URL = IS_DEVELOPMENT_ENV
     ? "http://localhost:3001"
-    : "https://audition-serverless.vercel.app";
+    : "https://api.audition.cbyrne.dev";
