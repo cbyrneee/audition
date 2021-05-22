58ef881f57b14961fee575686ae3e2154436429e
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2021-05-22 21:32:24 +0100

    [All] Remove react-loader-spinner
    This dependency did not work under Safari, and purely cosmetic.

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,22 +1,9 @@
 import React from "react";
-import Loader from "react-loader-spinner";
-import useMediaQuery from "../hooks/useMediaQuery";
 
 export default function LoadingComponent({ text }) {
     return (
         <div className="center-container">
             <h1 className="title">{text}</h1>
-
-            <Loader
-                type="ThreeDots"
-                height={80}
-                width={80}
-                color={
-                    useMediaQuery("(prefers-color-scheme: dark)")
-                        ? "#FFFFFF"
-                        : "#000000"
-                }
-            />
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import "./style/index.css";
 
 ReactDOM.render(
