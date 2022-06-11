fb493c8af5d4d3f2cc3b07bace9892ce0efa20bc
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2022-06-11 16:05:05 +0100

    feat: Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## audition
+
+Audition is a web application made with Next.JS that recommends artists on Spotify based on your music taste.
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Audition</title>
+      </Head>
+
+      <h1 className="text-4xl font-bold">Hello World</h1>
+    </>
+  );
+}
