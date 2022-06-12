3c84fe8107d9e118c9cc396f15e7a92288b3425c
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2022-06-12 13:45:20 +0100

    feat: Show logged in user information in nav bar

--- src/elements/base/navigation/Navigation.tsx
+++ src/elements/base/navigation/Navigation.tsx
@@ -1,13 +1,14 @@
 import { useSession } from 'next-auth/react';
-import Link from 'next/link';
 import React from 'react';
 import NavigationRoute from './NavigationRoute';
+import UserInfo from './UserInfo';
 
 export function Navigation() {
   const { data } = useSession();
   return (
     <div className="flex flex-row gap-4 justify-between">
       <NavigationRoute name="Audition" destination="/" />
+      <UserInfo />
     </div>
   );
 }
---
+++ src/elements/base/navigation/UserInfo.tsx
@@ -0,0 +1,56 @@
+import { signOut, useSession } from 'next-auth/react';
+import React, { Fragment } from 'react';
+import { Menu, Transition } from '@headlessui/react';
+import { ChevronDownIcon, LogoutIcon } from '@heroicons/react/outline';
+import Image from 'next/image';
+import typography from '../../../lib/typography';
+import styles from '../../../lib/styles';
+
+export default function UserInfo() {
+  const { data } = useSession();
+
+  if (!data?.user) {
+    return <></>;
+  }
+
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button
+          className={`inline-flex items-center w-full justify-center rounded-md p-4 py-2 ${typography.paragraph} ${styles.press}`}
+        >
+          <div className="flex flex-row items-center gap-2">
+            {data.user.image && <Image className="rounded-full" src={data.user.image} width="24px" height="24px" />}
+            <p>{data.user.name}</p>
+          </div>
+          <ChevronDownIcon className={`ml-2 -mr-1 h-5 w-5`} aria-hidden="true" />
+        </Menu.Button>
+      </div>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right rounded-md shadow-lg dark:bg-neutral-800 ring-1 ring-black dark:ring-neutral-700 ring-opacity-10 focus:outline-none">
+          <div className="px-1 py-1 ">
+            <Menu.Item>
+              <button
+                onClick={() => {
+                  signOut({ callbackUrl: '/' });
+                }}
+                className={`w-full group flex gap-1 items-center rounded-md px-2 py-2 ${typography.paragraph} ${styles.press}`}
+              >
+                <LogoutIcon className={`h-5 w-5`} />
+                Logout
+              </button>
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
+}
