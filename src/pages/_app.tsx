5d16c08c96dc0119ffc4549bff918b6d86c0fbc6
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2022-06-12 13:04:19 +0100

    chore: Wrap app in a `SessionProvider` for next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,17 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import { PageContainer } from '../components/layout/PageContainer';
 import { Footer } from '../elements/base';
+import { SessionProvider } from 'next-auth/react';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <ThemeProvider defaultTheme="dark" enableSystem={false} attribute="class">
-      <PageContainer>
-        <Component {...pageProps} />
-        <Footer />
-      </PageContainer>
-    </ThemeProvider>
+    <SessionProvider session={session}>
+      <ThemeProvider defaultTheme="dark" enableSystem={false} attribute="class">
+        <PageContainer>
+          <Component {...pageProps} />
+          <Footer />
+        </PageContainer>
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
