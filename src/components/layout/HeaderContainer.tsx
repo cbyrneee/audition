6fe8835d5c1988b9e5c73bae16a342f97c6ad0dd
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2022-06-12 13:04:07 +0100

    ui: Make `HeaderContainer` fill width of parent

--- src/components/layout/HeaderContainer.tsx
+++ src/components/layout/HeaderContainer.tsx
@@ -5,5 +5,5 @@ type HeaderContainerProps = {
 };
 
 export function HeaderContainer({ children }: HeaderContainerProps) {
-  return <div className="flex flex-col gap-2">{children}</div>;
+  return <div className="flex flex-col gap-2 w-full">{children}</div>;
 }
