e9d1b9e2a261f0448775611a91e701b2d68757d4
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2022-06-12 13:22:53 +0100

    ui: Improve style of card element

--- src/components/layout/Card.tsx
+++ src/components/layout/Card.tsx
@@ -7,10 +7,6 @@ type CardProps = {
 
 export function Card({ className, children }: CardProps) {
   return (
-    <div
-      className={`p-4 bg-neutral-100 border-neutral-200 dark:bg-neutral-800 border rounded-lg dark:border-neutral-700 ${className}`}
-    >
-      {children}
-    </div>
+    <div className={`p-4 border-neutral-200 rounded-lg border-2 dark:border-neutral-800 ${className}`}>{children}</div>
   );
 }
