79696722e5ed5602ead2d2508a4620493e084420
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2022-06-12 14:00:09 +0100

    chore: Don't show login button when logged in

--- src/elements/home/Header.tsx
+++ src/elements/home/Header.tsx
@@ -1,24 +1,31 @@
-import { signIn } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
 import React from 'react';
 import { Button } from '../../components/control';
 import { ButtonGroup, HeaderContainer } from '../../components/layout';
 import typography from '../../lib/typography';
 
 export function Header() {
+  const { data } = useSession();
   return (
     <HeaderContainer>
       <h1 className={typography.title}>Audition</h1>
       <h3 className={typography.subtitle}>Find new artists on Spotify based on your music taste!</h3>
 
       <ButtonGroup className="mt-4">
-        <Button
-          className="bg-green-400 text-green-900 dark:bg-green-500"
-          onClick={() => {
-            signIn('spotify', { callbackUrl: '/artists' });
-          }}
-        >
-          Login with Spotify
-        </Button>
+        {!data ? (
+          <Button
+            className="bg-green-400 text-green-900 dark:bg-green-500"
+            onClick={() => {
+              signIn('spotify', { callbackUrl: '/artists' });
+            }}
+          >
+            Login with Spotify
+          </Button>
+        ) : (
+          <Button destination="/artists" className="bg-blue-400 text-blue-900 dark:bg-blue-500">
+            View Artists
+          </Button>
+        )}
       </ButtonGroup>
     </HeaderContainer>
   );
