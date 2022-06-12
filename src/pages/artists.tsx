89f6b4bf7e3fd62d2a61470a6a8ba4349d4e6f30
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2022-06-12 13:11:12 +0100

    feat(artists): Show logged in user information

--- src/pages/artists.tsx
+++ src/pages/artists.tsx
@@ -1,17 +1,26 @@
-// import { useSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import React from 'react';
 import { Auth } from '../components/base';
 import { HeaderContainer } from '../components/layout';
 import typography from '../lib/typography';
+import { UserInfo } from '../elements/artists';
 
 export default function Artists() {
-  // const { data } = useSession({ required: true });
+  const { data } = useSession({ required: true });
+
+  if (!data || !data.user) {
+    return <></>;
+  }
 
   return (
     <Auth>
       <HeaderContainer>
         <h1 className={typography.title}>Artists</h1>
         <h3 className={typography.subtitle}>Here's some recommended artists based on your listening activity.</h3>
+
+        <div className="md:flex mt-4">
+          <UserInfo session={data} />
+        </div>
       </HeaderContainer>
     </Auth>
   );
