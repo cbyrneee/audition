54bc6e1ad100c1e0f296171b8f0e6338f892cc46
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2022-06-13 19:42:30 +0100

    fix: Remove `hover:opacity-70` from paragraph styles

--- src/lib/typography.ts
+++ src/lib/typography.ts
@@ -1,7 +1,7 @@
 const typography = {
   title: 'text-4xl font-bold',
   subtitle: 'text-xl font-medium text-neutral-700 dark:text-neutral-400',
-  paragraph: 'text-neutral-700 transition-all hover:opacity-70 dark:text-neutral-400',
+  paragraph: 'text-neutral-700 transition-all dark:text-neutral-400',
   headline: 'font-extrabold uppercase text-neutral-500',
 };
 
